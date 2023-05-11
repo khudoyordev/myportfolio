db2a54dfeaa46a85ac50d85bfc070aeb0f35063c
khudoyordev <nasima.sharipova1967@gmail.com>
2023-05-12 02:05:30 +0500

    finished portfolio

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
+window.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    const header = document.querySelector(".header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+  });
 
+  const menuBtn = document.querySelector(".menu-btn");
+  const navBtn = document.querySelector(".navbar");
+  const navLink = document.querySelectorAll(".nav-link");
+  const scrollBtn = document.querySelector(".scrollToBtn");
+
+  menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    navBtn.classList.toggle("active");
+  });
+
+  navLink.forEach((link) => {
+    link.addEventListener("click", () => {
+      menuBtn.classList.remove("active");
+      navBtn.classList.remove("active");
+    });
+  });
+
+  window.addEventListener("scroll", () => {
+    scrollBtn.classList.toggle("active", window.scrollY > 50);
+  });
+  scrollBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
+
+  window.addEventListener("scroll", () => {
+    let reveals = document.querySelectorAll(".reveal");
+
+    for (let i = 0; i < reveals.length; i++) {
+      let windowHead = window.innerHeight;
+      let revealTop = reveals[i].getBoundingClientRect().top;
+      let revealPoint = 50;
+      if (revealTop < windowHead - revealPoint) {
+        reveals[i].classList.add("active");
+      }
+    }
+  });
+});
